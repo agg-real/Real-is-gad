0e238a6a9a8cbeed417c98980a296f507397cc87
agg-real <agg603450@gmail.com>
2025-04-05 15:06:18 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -10,7 +10,6 @@ local HttpService = game:GetService("HttpService")
 local Window = OrionLib:MakeWindow({Name = "Skibi-lich hub", HidePremium = false, SaveConfig = true})
 local Tab = Window:MakeTab({Name = "Главная", Icon = "rbxassetid://89991291117147"})
 local power = 50
-local glove = "Brick"
 local flight = 0.52
 local speed = 0
 local flightEnabled = false
